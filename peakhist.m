7ed8a03ac6b595d8c583e8a2d0314aa1781ba2f7
Alan Bush <alan.bush@mgh.harvard.edu>
2021-05-12 13:23:17 -0400

    cleanup by Witek

--- peakhist.m
+++
@@ -1,9 +0,0 @@
-function peakhist(H, filename)
-
-[S, vo] = spikehist(H);
-
-figure;
-plot(vo, S, 'Marker', '.', 'MarkerSize', 5);
-xlabel('mV');
-ylabel('cts/bin');
-title([filename, ' Peak histogram']);
